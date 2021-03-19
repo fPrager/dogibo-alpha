4e1b3586af468ee7c4b5b6017ed4e18d036e2a6c
fPrager <wesayso+github@pm.me>
2021-03-19 14:50:02 +0100

    style setup

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+import { GeistProvider, CssBaseline, Page } from '@geist-ui/react';
+import DefaultTheme from './themes';
+
+import '../styles/main.scss';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GeistProvider themes={[DefaultTheme]} themeType="default">
+      <CssBaseline />
+      <Page>
+        <Page.Content>
+          <Component {...pageProps} />
+        </Page.Content>
+      </Page>
+    </GeistProvider>
+  </>
+);
+
+export default App;
---
+++ pages/themes.ts
@@ -0,0 +1,28 @@
+/**
+ * Just customize what you need, deep merge themes by default.
+ *
+ * If you are using TypeScript, please use the following type definition.
+ * If you are using JavaScript, refer to https://github.com/geist-org/react/blob/master/components/themes/presets/index.ts
+ */
+
+import {
+//   GeistUIThemes,
+//   GeistUIThemesPalette,
+//   GeistUIThemesExpressiveness,
+Themes,
+GeistUIThemes,
+GeistUIThemesFont,
+} from '@geist-ui/react';
+
+
+const font: GeistUIThemesFont = {
+    mono: "SmoothySans",
+    sans: "SmoothySans",
+};
+
+const DefaultTheme = Themes.createFromLight({
+    type: 'default',
+    font,
+});
+
+export default DefaultTheme;
